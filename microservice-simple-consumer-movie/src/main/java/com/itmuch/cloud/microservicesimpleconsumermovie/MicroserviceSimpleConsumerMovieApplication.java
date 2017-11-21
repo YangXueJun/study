3394a3256266435ba93f898622ef44ad387bfed4
杨学君 <yangxuejun@yangxuejundeMacBook-Pro.local>
2017-11-21 20:16:23 +0800

    add folder

---
+++ microservice-simple-consumer-movie/src/main/java/com/itmuch/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
@@ -0,0 +1,12 @@
+package com.itmuch.cloud.microservicesimpleconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleConsumerMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
+	}
+}
---
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/microservicesimpleprovideruser/MicroserviceSimpleProviderUserApplication.java
@@ -0,0 +1,12 @@
+package com.itmuch.cloud.microservicesimpleprovideruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleProviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleProviderUserApplication.class, args);
+	}
+}
